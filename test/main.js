d7491eb00377a040b02f8d86af851d89015c8ad4
springuper <springuper@gmail.com>
2017-04-11 19:04:43 +0800

    add more tests

--- test/main.js
+++ test/main.js
@@ -1,16 +1,18 @@
+'use strict';
+
 var assert = require('assert');
-var Promise = require('es6-promise').Promise;
 var promiseParallel = require('../lib/concurrency');
 
 describe('PromiseParallel', function () {
     it('should execute promises in parallel', function (done) {
         var counter = 0;
         var sequence = [];
+        var LIMIT = 2;
         var promiseFactories = [5, 1, 2, 3, 4].map(function (item) {
             return function () {
                 return new Promise(function (resolve) {
                     counter++;
-                    assert(counter <= 2);
+                    assert(counter <= LIMIT);
                     setTimeout(function () {
                         counter--;
                         assert(counter >= 0);
@@ -20,7 +22,7 @@ describe('PromiseParallel', function () {
                 });
             };
         });
-        promiseParallel(promiseFactories, 2).then(function (value) {
+        promiseParallel(promiseFactories, LIMIT).then(function (value) {
             assert.deepEqual(value, [5, 1, 2, 3, 4]);
             assert.deepEqual(sequence, [1, 2, 5, 3, 4]);
             done();
@@ -33,5 +35,67 @@ describe('PromiseParallel', function () {
             assert.equal(reason.message, 'promise list is empty.');
             done();
         });
+    });
+
+    it('should stop and reject when some promise fails', function (done) {
+        var sequence = [];
+        var LIMIT = 2;
+        var promiseFactories = [1, 2, 3, 4, 5].map(function (item) {
+            return function () {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(function () {
+                        if (item === 3) {
+                            reject('Wrong value.');
+                        } else {
+                            sequence.push(item);
+                            resolve(item);
+                        }
+                    }, 100);
+                });
+            };
+        });
+        promiseParallel(promiseFactories, LIMIT).then(function () {
+            throw new Error('Unexpected branch.');
+        }, function (reason) {
+            assert.deepEqual(reason, 'Wrong value.');
+            assert.deepEqual(sequence, [1, 2, 4]);
+            done();
+        }).catch(done);
+    });
+
+    it('should reject only one time when multiple promises fail', function (done) {
+        var sequence = [];
+        var LIMIT = 2;
+        var rejectCount = 0;
+        var doneTimer = null;
+        var promiseFactories = [1, 2, 3, 4, 5].map(function (item) {
+            return function () {
+                return new Promise(function (resolve, reject) {
+                    setTimeout(function () {
+                        if (item === 3 || item === 4) {
+                            reject('Wrong value.');
+                        } else {
+                            sequence.push(item);
+                            resolve(item);
+                        }
+                    }, 100);
+                });
+            };
+        });
+        promiseParallel(promiseFactories, LIMIT).then(function () {
+            throw new Error('Unexpected branch.');
+        }, function (reason) {
+            rejectCount++;
+            assert.deepEqual(reason, 'Wrong value.');
+            assert.deepEqual(sequence, [1, 2]);
+
+            if (doneTimer) {
+                clearTimeout(doneTimer);
+            }
+            doneTimer = setTimeout(function () {
+                assert.equal(rejectCount, 1);
+                done();
+            }, 500);
+        }).catch(done);
     });
 });
